ae83176f7b99f5946bca3fc732c1bc2184b6cfb4
sewinter <samuelewinter@gmail.com>
2023-01-22 20:06:53 -0500

    hmm

--- crates/revm/src/evm_impl.rs
+++ crates/revm/src/evm_impl.rs
@@ -77,15 +77,15 @@ impl<'a, GSPEC: Spec, DB: Database, const INSPECT: bool> Transact<DB::Error>
             // check if priority fee is lower then max fee
         }
 
-        #[cfg(feature = "optional_block_gas_limit")]
-        let disable_block_gas_limit = self.env().cfg.disable_block_gas_limit;
-        #[cfg(not(feature = "optional_block_gas_limit"))]
-        let disable_block_gas_limit = false;
+        // #[cfg(feature = "optional_block_gas_limit")]
+        // let disable_block_gas_limit = self.env().cfg.disable_block_gas_limit;
+        // #[cfg(not(feature = "optional_block_gas_limit"))]
+        // let disable_block_gas_limit = false;
 
-        // unusual to be found here, but check if gas_limit is more then block_gas_limit
-        if !disable_block_gas_limit && U256::from(gas_limit) > self.data.env.block.gas_limit {
-            return Err(InvalidTransaction::CallerGasLimitMoreThenBlock.into());
-        }
+        // // unusual to be found here, but check if gas_limit is more then block_gas_limit
+        // if !disable_block_gas_limit && U256::from(gas_limit) > self.data.env.block.gas_limit {
+        //     return Err(InvalidTransaction::CallerGasLimitMoreThenBlock.into());
+        // }
 
         // load acc
         self.data
